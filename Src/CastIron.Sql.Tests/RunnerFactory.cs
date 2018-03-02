20b0ceca2fcafadd1ef4cf6a3b050c4b7ee87da3
Andrew F. Whitworth <wknight8111@gmail.com>
2018-03-02 16:56:07 -0500

    Use proper configuration, with per-platform config files for unit test project
    do not hard-code connection strings because the windows one doesn't work on linux and vice-versa

--- Src/CastIron.Sql.Tests/RunnerFactory.cs
+++ Src/CastIron.Sql.Tests/RunnerFactory.cs
@@ -1,11 +1,30 @@
-﻿namespace CastIron.Sql.Tests
+﻿using System.IO;
+using System.Runtime.InteropServices;
+using Microsoft.Extensions.Configuration;
+
+namespace CastIron.Sql.Tests
 {
     public static class RunnerFactory
     {
+        private readonly static string _connectionString;
+
+        static RunnerFactory()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory());
+
+            if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                builder = builder.AddJsonFile("appsettings.linux.json");
+            else if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                builder = builder.AddJsonFile("appsettings.windows.json");
+
+            var configuration = builder.Build();
+            _connectionString = configuration["ConnectionString"];
+        }
+
         public static SqlQueryRunner Create()
         {
-            //return new SqlQueryRunner("server=localhost;Integrated Security=SSPI;");
-            return new SqlQueryRunner("server=localhost;user id=SA;password=passwordABC123;");
+            return new SqlQueryRunner(_connectionString);
         }
     }
 }
