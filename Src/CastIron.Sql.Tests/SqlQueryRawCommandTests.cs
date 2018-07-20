f05f06b64d6dbbbab605869d23f13bd18c90e86e
awhitworth <awhitworth@asicentral.com>
2018-07-20 11:23:24 -0400

    Extension methods to aid in parameter setting

--- Src/CastIron.Sql.Tests/SqlQueryRawCommandTests.cs
+++ Src/CastIron.Sql.Tests/SqlQueryRawCommandTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using FluentAssertions;
 using NUnit.Framework;
---
+++ Src/CastIron.Sql/DbCommandExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Data;
+
+namespace CastIron.Sql
+{
+    public static class DbCommandExtensions
+    {
+        public static void AddParameterWithValue(this IDbCommand command, string name, object value)
+        {
+            var param = command.CreateParameter();
+            param.ParameterName = name;
+            param.Value = value;
+            command.Parameters.Add(param);
+        }
+    }
+}
