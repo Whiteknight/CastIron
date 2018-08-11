b508dab3f6bc37951d57baf5d031a577e21a5a27
Andrew F. Whitworth <wknight8111@gmail.com>
2018-08-11 06:47:40 -0400

    Update build script to cover all libs

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
  dotnet build -f netstandard2.0 Src/CastIron.Sql/CastIron.Sql.csproj
+ dotnet build --no-dependencies -f netstandard2.0 Src/CastIron.MySql/CastIron.MySql.csproj
+ dotnet build --no-dependencies -f netstandard2.0 Src/CastIron.Postgres/CastIron.Postgres.csproj
+ dotnet build --no-dependencies -f netstandard2.0 Src/CastIron.Sqlite/CastIron.Sqlite.csproj
+
  dotnet build --no-dependencies -f netcoreapp2.0 Src/CastIron.Sql.Tests/CastIron.Sql.Tests.csproj
