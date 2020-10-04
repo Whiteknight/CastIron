7bd90744ed5ddf334c4d33b62653f3aa92b4bfc8
Whiteknight <wknight8111@gmail.com>
2020-10-04 13:38:20 -0400

    start cleanup of dictionary mapping tests

--- Src/CastIron.Postgres.Tests/Mapping/AbstractDictionaryMappingTests.cs
+++ Src/CastIron.Postgres.Tests/Mapping/AbstractDictionaryMappingTests.cs
@@ -6,34 +6,9 @@ using NUnit.Framework;
 
 namespace CastIron.Postgres.Tests.Mapping
 {
-    [TestFixture]
-    public class DictionaryMappingTests
+
+    public class AbstractDictionaryMappingTests
     {
-        [Test]
-        public void Map_DictionaryOfObject()
-        {
-            var target = RunnerFactory.Create();
-            var result = target.Query<Dictionary<string, object>>("SELECT 5 AS TestInt, 'TEST' AS TestString, CAST(1 AS BIT) AS TestBool;").First();
-            result.Count.Should().Be(3);
-            result["teststring"].Should().Be("TEST");
-            result["testint"].Should().Be(5);
-            result["testbool"].Should().Be(true);
-        }
-
-        [Test]
-        public void Map_DictionaryOfObjectWithDuplicates()
-        {
-            var target = RunnerFactory.Create();
-            var dict = target.Query<Dictionary<string, object>>("SELECT 5 AS TestInt, 'A' AS TestString, 'B' AS TestString;").First();
-
-            dict.Count.Should().Be(2);
-            dict["testint"].Should().Be(5);
-            dict["teststring"].Should().BeOfType<object[]>();
-            var array = dict["teststring"] as object[];
-            array[0].Should().Be("A");
-            array[1].Should().Be("B");
-        }
-
         [Test]
         public void Map_IDictionaryOfObject()
         {
@@ -70,12 +45,6 @@ namespace CastIron.Postgres.Tests.Mapping
             result["testbool"].Should().Be(true);
         }
 
-        public class TestObject_ChildDict
-        {
-            public int Id { get; set; }
-            public Dictionary<string, string> Child { get; set; }
-        }
-
         [Test]
         public void Map_IReadOnlyDictionaryOfObjectWithDuplicates()
         {
@@ -88,29 +57,6 @@ namespace CastIron.Postgres.Tests.Mapping
             var array = dict["teststring"] as object[];
             array[0].Should().Be("A");
             array[1].Should().Be("B");
-        }
-
-        [Test]
-        public void Map_DictionaryOfListOfInt()
-        {
-            var target = RunnerFactory.Create();
-            var dict = target.Query<Dictionary<string, List<int>>>("SELECT 1 AS A, 2 AS A, 3 AS B, 4 AS B, 5 AS C;").First();
-
-            dict.Count.Should().Be(3);
-            dict["a"].Should().ContainInOrder(1, 2);
-            dict["b"].Should().ContainInOrder(3, 4);
-            dict["c"].Should().ContainInOrder(5);
-        }
-
-        [Test]
-        public void Map_CustomObjectWithChildDictionary()
-        {
-            var target = RunnerFactory.Create();
-            var result = target.Query<TestObject_ChildDict>("SELECT 5 AS Id, 'TEST' AS Child_String, 6 AS Child_Int;").First();
-            result.Id.Should().Be(5);
-            result.Child.Count.Should().Be(2);
-            result.Child["string"].Should().Be("TEST");
-            result.Child["int"].Should().Be("6");
         }
 
         public class TestObject_ChildIDict
